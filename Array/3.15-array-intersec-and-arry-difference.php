ff12aa44fa6d2519be8e021a565c03d0a3c6c07d
Shamim448 <shamim.448@gmail.com>
2021-02-11 08:58:01 +0600

    array Intersec

---
+++ Array/3.15-array-intersec-and-arry-difference.php
@@ -0,0 +1,18 @@
+<?php
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"green","g"=>"blue");
+//Compare the values of two arrays, and return the matches
+$result=array_intersect($a1,$a2);
+print_r($result);
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("a"=>"red","b"=>"green","c"=>"blue");
+//Compare the keys and values of two arrays, and return the matches:
+$result=array_intersect_assoc($a1,$a2);
+print_r($result);
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"green","g"=>"blue");
+//Compare the values of two arrays, and return the differences:
+$result=array_diff($a1,$a2);
+print_r($result);
+?>
