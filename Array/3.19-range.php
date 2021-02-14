1d0fffc4bc364660a2f0349c6889a170875e5311
Shamim448 <shamim.448@gmail.com>
2021-02-14 13:06:08 +0600

    Project 1 setup, using miligrame with normalize css

--- Array/3.19-range.php
+++ Array/3.19-range.php
@@ -2,9 +2,8 @@
 //0==start value, 15=end value, 3=steaping
 $number = range(0,15, 3);
 print_r ($number);
+
 //shuffel
-
 $my_array = array("red","green","blue","yellow","purple");
-
 shuffle($my_array);
 print_r($my_array[3]);
---
+++ Project-1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column colums-60 column-offset-20">
+                <h1>Welcome</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptates alias fugiat veritatis quam quia, asperiores, numquam accusantium eligendi dolores natus qui, doloribus molestias et ullam consequatur dolor? Officiis repudiandae quidem aut deleniti vel quam praesentium. Est voluptatibus aut officia nobis cupiditate fuga rem. Sed perferendis accusamus saepe perspiciatis ipsam.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
